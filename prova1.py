0373646f6eb65d79751f6a049875d5a974d55440
jcarreras <jcarreras@iesjoanramis.org>
2025-11-12 13:35:07 +0100

    ex1.b

--- prova1.py
+++ prova1.py
@@ -3,6 +3,11 @@ b = float(input("Escriure el segon nombre: "))
 d = float(input("Escriure el tercer nombre: "))
 c = a + b + d
 print("El resultat de la suma {} + {} + {} és {}".format(a, b, d, c))
+if c>20:
+    print("La suma és major que 20, que és {}".format(c))
+else:
+    print("La suma és menor que 20, que és {}".format(c))
+    
 c = a * b * d
 print("El resultat de la multiplicació {} * {} * {} és {}".format(a, b, d, c))
 
