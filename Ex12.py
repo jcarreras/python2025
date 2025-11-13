2a7bc623f96ed98e9889a22e0511f510849569d3
jcarreras <jcarreras@iesjoanramis.org>
2025-11-13 11:51:53 +0100

    ex12.c

--- Ex12.py
+++ Ex12.py
@@ -1,7 +1,12 @@
+def majoredat(edat):
+    if edat > 18:
+        print("És major d'edat")
+    elif edat<18:
+        print("És menor d'edat")
+    else:
+        print("Enhorabona, aquest any has fet 18 anys")
+
 edat = int(input("Escriu la seva edat: "))
-if edat > 18:
-    print("És major d'edat")
-elif edat<18:
-    print("És menor d'edat")
-else:
-    print("Enhorabona, aquest any has fet 18 anys")
+majoredat(edat)
+edat = int(input("Escriu la seva edat: "))
+majoredat(edat)
